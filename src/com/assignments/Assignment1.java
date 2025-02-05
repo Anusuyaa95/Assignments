905d13975ec45df055f39e666b28b04c97c1bfba
RanjithaRajkumar <ranjitharajkumar123@gmail.com>
2025-02-05 17:44:22 +0530

    Assignment1 - DataTypes

--- src/com/assignments/Assignment1.java
+++ src/com/assignments/Assignment1.java
@@ -3,7 +3,33 @@ package com.assignments;
 public class Assignment1 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		float temperature = 25.5f;
+		boolean orderPlaced = true;
+		String phoneNumber = "123-456-7890";
+		float amount = 1000.50f;
+		String emailAddress = "john.doe@example.com";
+		double[] lattitudeAndLongitude = {37.77749,-122.4194};
+		boolean maritalStatus = true;
+		String occupation = "Software Engineer";
+		String favColour = "Blue";
+		short currentYear = 2023;
+		int[] followers = {1,000,000};
+		float movieRating = 7.5f;
+		char bloodType = 'A';
+		String title = "To kill a Mockingbird";
+		int employees = 500;
+		String eventTime = "2:30 PM";
+		String country = "United States";
+		String eyeColor = "Brown";
+		String birthPlace = "New York City";
+		float distance = 200.5f;
+		
+		
+
+		
+		
+		
 
 	}
 
